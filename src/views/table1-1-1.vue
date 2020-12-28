345eeeed506e114ddf0744bd14d122f0f1faf1b1
zhangsongshen <zhangsongshen@minke.cn>
2020-12-28 15:00:01 +0800

    demo

--- src/views/table1-1-1.vue
+++ src/views/table1-1-1.vue
@@ -7,55 +7,17 @@
 -->
 
 <template>
-    <div id='table1-1-1'>
-        <el-table
-                :data="tableData"
-                style="width: 100%">
-            <el-table-column
-                    prop="date"
-                    label="日期"
-                    width="180">
-            </el-table-column>
-            <el-table-column
-                    prop="name"
-                    label="姓名"
-                    width="180">
-            </el-table-column>
-            <el-table-column
-                    prop="address"
-                    label="地址">
-            </el-table-column>
-        </el-table>
-    </div>
+    <TableDemo></TableDemo>
 </template>
 
 <script>
-import { ref } from 'vue'
+import TableDemo from './table.vue'
 export default {
+    components: {
+        TableDemo
+    },
     setup() {
-        let tableData = ref([
-            {
-                date: '2016-05-02',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1518 弄'
-            }, {
-                date: '2016-05-04',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1517 弄'
-            }, {
-                date: '2016-05-01',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1519 弄'
-            }, {
-                date: '2016-05-03',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1516 弄'
-            }
-        ])
 
-        return {
-            tableData
-        }
     }
 }
 </script>
